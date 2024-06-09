d7601eef82eeff12f258bd9607218904ba88070a
Alexey <alexshamox@gmail.com>
2024-06-09 18:59:53 +0500

    first commit

---
+++ src/main/java/ru/shtamov/testUISUP/TestUisupApplication.java
@@ -0,0 +1,13 @@
+package ru.shtamov.testUISUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUisupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUisupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shtamov/testUISUP/TestUisupApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shtamov.testUISUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUisupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
