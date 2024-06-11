c2689daf056acc3d226704cf5dfba54afd2494c3
Alexey <alexshamox@gmail.com>
2024-06-11 14:47:30 +0500

    add data.sql and redirect link buttons

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO resp_person (uuid, fullname) VALUES
+('cbcf3224-d643-48c3-9e10-d288dc8088f6', 'Иванов Иван Иванович'),
+('4e8b8bd8-bd72-4ef0-8437-1f7e65ebcd9f', 'Петров Петр Петрович'),
+('d9d083c5-e830-41f1-ad9d-31610b6e1a1c', 'Сидоров Сидор Сидорович');
+
+INSERT INTO institute (uuid, title) VALUES
+('680c6f88-0aba-441a-a3c1-c21aea1c07d5', 'Институт 1'),
+('8de118d6-5c40-4788-9c15-64fb8a88502f', 'Институт 2'),
+('b34dd0b0-e6e2-4385-b42c-9303a1eaecb0', 'Институт 3');
